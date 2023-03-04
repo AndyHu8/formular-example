a7009fd438ec3fd11f32fb2171770bb54aa97368
AndyHu8 <huandy01@gmail.com>
2023-03-05 00:26:04 +0100

    Footer fertig

---
+++ form/src/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+import logo from "../Bilder/CMORA_weiss.png";
+
+export default function Footer() {
+    return (
+        <div className="footerContainer">
+            <div className="footerLinks">
+                <h3>ENTWICKELT VON <span onClick={() => window.open("https://www.xiaohu.de/")}>XIAØHU</span></h3>
+                <h3>Mit Liebe <br/>
+                & Leidenschaft</h3>
+            </div>
+            <div className="footerRechts">
+                <img src={logo} alt="Palmen aus Pappe" />
+                <h2>2023 Palmen aus Pappe</h2>
+            </div>
+        </div>
+    )
+}
