37c92f20f88aa6b3da42416dd31904539c3f7b26
AndyHu8 <huandy01@gmail.com>
2023-03-04 20:50:46 +0100

    Navigation fertig

---
+++ form/src/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import "./Navigation.css"
+
+export default function Navigation() {
+    return (
+        <div className="navigationContainer">
+            <div className="logo" onClick={() => window.location.reload()}>Palmen aus Pappe</div>
+            <ul className="list">
+                <a href="#home"><li>Home</li></a>
+                <a href="#videos"><li>Videos</li></a>
+                <a href="#form"><li>Form</li></a>
+            </ul>
+        </div>
+    )
+}
