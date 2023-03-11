336205f86fd185ec4e91e4d1fbc08de4c5eb5cba
AndyHu8 <huandy01@gmail.com>
2023-03-12 00:10:13 +0100

    Form fix

--- form/src/Form/Form.jsx
+++ form/src/Form/Form.jsx
@@ -3,7 +3,7 @@ import "./Form.css";
 export default function Form() {
     return (
         <div id="form" className="formContainer">
-            <form action="'https://formsubmit.co/huandy01@gmail.com" method="POST">
+            <form action="https://formsubmit.co/huandy01@gmail.com" method="POST">
                 <label>Du hast noch Fragen? Dann kontaktiere uns!</label>
                 <input name="email" type="email" placeholder="Deine Email-Adresse" required/>
                 <input name="name" type="text" placeholder="Dein Name" required/>
