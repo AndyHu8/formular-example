f13c51e8e82477d11b50d2269a2e7480105d09c7
AndyHu8 <huandy01@gmail.com>
2023-04-05 13:44:47 +0200

    Videos RPSV

--- form/src/Videos/Videos.jsx
+++ form/src/Videos/Videos.jsx
@@ -5,14 +5,11 @@ export default function Videos() {
         <div id="videos" className="videosContainer">
             <h1>Die besten Videos aus Palmen aus Pappe 3</h1>
             <div>
-                <iframe width="450" height="300"
-                    src="https://www.youtube.com/embed/ymfftdIQOh0">
+                <iframe src="https://www.youtube.com/embed/ymfftdIQOh0">
                 </iframe>
-                <iframe width="450" height="300"
-                    src="https://www.youtube.com/embed/wbLpd_fquTg">
+                <iframe src="https://www.youtube.com/embed/wbLpd_fquTg">
                 </iframe>
-                <iframe width="450" height="300"
-                    src="https://www.youtube.com/embed/BKGeueSkWXQ">
+                <iframe src="https://www.youtube.com/embed/BKGeueSkWXQ">
                 </iframe>
             </div>
         </div>
